c9f96d528490811dcd9e1b0957f1c553ca660f00
awiana1 <ebtiamzon@up.edu.ph>
2025-03-10 18:09:54 +0800

    Feat: Added a addInfo function which displays the user's input. Changed the algorithm.

--- static_files/dom-script.js
+++ static_files/dom-script.js
@@ -1,116 +1,85 @@
-//  var animesLabel = document.getElementById("animesLabel");
-//  console.log(animesLabel);
 
-//  var listItems = document.getElementsByTagName("li");
-//  console.log(listItems);
-//  console.log(listItems.length);
-//  console.log(listItems[0]);
-//  console.log(listItems["aot"]);
+// // MODIFICATION DEMO
+// var animesLabel = document.getElementById("animesLabel");
+// animesLabel.innerText = "Favorite Animes";     // For 
 
-//  var container = document.getElementsByClassName("container");
-//  console.log(containter);
+// var gamesLabel = document.getElementById("gamesLabel");
+// gamesLabel.innerText = "Favorite Games";
 
-// MODIFICATION DEMO
-var animesLabel = document.getElementById("animesLabel");
-animesLabel.innerText = "Favorite Animes";     // For 
-
-var gamesLabel = document.getElementById("gamesLabel");
-gamesLabel.innerText = "Favorite Games";
-
-var jjk = document.getElementById("jjk");
-jjk.innerHTML = <h3>${jjk.innerText}</h3>;
+// var jjk = document.getElementById("jjk");
+// jjk.innerHTML = <h3>${jjk.innerText}</h3>;
 
 
-var games = document.getElementsByClassName("game");
-for (var i = 0; i < games.length; i++) {
-  games[i].style = "font-size: 30px";
-}
+// var games = document.getElementsByClassName("game");
+// for (var i = 0; i < games.length; i++) {
+//   games[i].style = "font-size: 30px";
+// }
 
-//
-var topImage = document.getElementById("main-art");
-topImage.removeAttribute("width");
+// //
+// var topImage = document.getElementById("main-art");
+// topImage.removeAttribute("width");
 
-// REMOVEING ELEMENT FROM A LIST
+// // REMOVEING ELEMENT FROM A LIST
 
-// // FIRST WAY
+// // // FIRST WAY
+// // var animeList = document.getElementById("animeList");
+// // animeList.removeChild(animeList.children[3]);
+
+// // SECOND WAY
+// // var frieren = document.getElementById("frieren");
+// // frieren.parentNode.removeChild(frieren);
+
+
+// // EVENTS
+// function greet() {
+//   var nameInput = document.getElementById("name");
+//   var name = nameInput.value;
+//   alert(`Hello ${name}!!!!`);
+//   alert("Hello po!");
+//   var addButton = document.getElementById("addButton");
+//   addButton.removeEventListener("click", greet);
+//   addButton.addEventListener("click", addElement);
+// }
+
+// // ADD ELEMENT TO A PARENT NODE
+// var newAnime = document.createElement("li");
+// newAnime.id = "plasticMemories";
+// newAnime.innerText = "Plastic Memories";
+// newAnime.className = "anime";
+
 // var animeList = document.getElementById("animeList");
-// animeList.removeChild(animeList.children[3]);
-
-// SECOND WAY
-// var frieren = document.getElementById("frieren");
-// frieren.parentNode.removeChild(frieren);
-
-// ADD ELEMENT TO A PARENT NODE
-var newAnime = document.createElement("li");
-newAnime.id = "plasticMemories";
-newAnime.innerText = "Plastic Memories";
-newAnime.className = "anime";
-
-var animeList = document.getElementById("animeList");
-animeList.appendChild(newAnime);
-
-// EVENTS
-function greet() {
-  var nameInput = document.getElementById("name");
-  var name = nameInput.value;
-  alert(`Hello ${name}!!!!`);
-  alert("Hello po!");
-  var addButton = document.getElementById("addButton");
-  addButton.removeEventListener("click", greet);
-  addButton.addEventListener("click", addElement);
-}
+// animeList.appendChild(newAnime);
 
 
 function addInfo() {
 
-  // for rank
-  var rankInput = document.getElementById("rank");
-  var rank = rankInput.value;
+  // returns the value based on user's input
+  var newRank = document.getElementById("rank").value;
+  var newName = document.getElementById("name1").value;
+  var newDesc = document.getElementById("description").value;
+  var newImage = document.getElementById("url").value;
 
-  newBox = document.createElement("li")
-  newBox.id = rank.toLowerCase();
-  newBox.innerText = newBox;
-  newBox.className = "inputBox";
+  var forInputList = document.createElement("li");
 
-  storeBox = document.getElementById("infoList");
-  storeBox.appendChild(newBox);
+  var rankInput = document.createElement("div");
+  rankInput.innerText = newRank;
 
-  // for name
-  var nameInput = document.getElementById("name1");
-  var name = nameInput.value;
+  var nameInput = document.createElement("div");
+  nameInput.innerText = newName;
 
-  var newBox = document.createElement("li")
-  newBox.id = name.toLowerCase();
-  newBox.innerText = newBox;
-  newBox.className = "inputBox";
+  var descInput = document.createElement("div");
+  descInput.innerText = newDesc;
 
-  storeBox = document.getElementById("infoList");
-  storeBox.appendChild(newBox);
+  var imageInput = document.createElement("div");
+  imageInput.innerText = newImage;
 
-  // for description
-  var descInput = document.getElementById("description");
+  forInputList.appendChild(rankInput);
+  forInputList.appendChild(nameInput);
+  forInputList.appendChild(descInput);
+  forInputList.appendChild(imageInput);
 
-  var desc = descInput.value;
-  var newBox = document.createElement("li")
-  newBox.id = descInput.toLowerCase();
-  newBox.innerText = newBox;
-  newBox.className = "inputBox";
-
-  storeBox = document.getElementById("infoList");
-  storeBox.appendChild(newBox);
-
-
-  var imageInput = document.getElementById("url");
-  var url = imageInput.value;
-
-  var newBox = document.createElement("li")
-  newBox.id = url.toLowerCase();
-  newBox.innerText = newBox;
-  newBox.className = "inputBox";
-  newBox.src = name;
-
-  storeBox = document.getElementById("infoList");
-  storeBox.appendChild(newBox);
+  var boxList = document.getElementById("infoList");
+  boxList.appendChild(forInputList)
 
 }
 
